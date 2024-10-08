62a9e4a48876084e4da24753e758265fbf356459
Chaudhary-Muhammad-Irfan <chirfan2002muhammad@gmail.com>
2024-10-08 20:57:39 +0500

    Created an object having a function and than created an other object and make first object prototype of this object

--- app.js
+++ app.js
@@ -6,3 +6,17 @@ const Student = {
         console.log(`Name is ${name} CGPA is ${cgpa} Department is ${this.department}`)
     }
 }
+
+// Creating a class that will have a function and we will use that function in the prototype of an other class.
+
+const Employee = {
+    taxOnSalary() {
+        console.log("The tax on salry is 10%");
+    }
+};
+
+// Now create an employee that has salary as variable and uses this class as prototype.
+const Chaudhary ={
+    salary:4000
+};
+Chaudhary.__proto__=Employee;
